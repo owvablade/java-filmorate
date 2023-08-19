8400d287b7756170815202c09d95e8215852aceb
Artem <av.cherep@yandex.ru>
2023-08-20 01:18:15 +0300

    feat: добавить логирование для контроллеров

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -20,13 +22,16 @@ public class FilmController {
 
     @PostMapping
     public Film add(@Valid @RequestBody Film film) {
+        log.info("Invoke add film method at resource POST /films = {}", film);
         return storage.add(film);
     }
 
     @PutMapping
     @ResponseBody
     public Film update(@Valid @RequestBody Film film, HttpServletResponse response) {
+        log.info("Invoke update film method at resource PUT /films = {}", film);
         if (storage.update(film) == null) {
+            log.info("Cannot update because film with id={} not found", film.getId());
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
         return film;
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,13 +22,16 @@ public class UserController {
 
     @PostMapping
     public User add(@Valid @RequestBody User user) {
+        log.info("Invoke add user method at resource POST /users = {}", user);
         return storage.add(user);
     }
 
     @PutMapping
     @ResponseBody
     public User update(@Valid @RequestBody User user, HttpServletResponse response) {
+        log.info("Invoke update user method at resource PUT /users = {}", user);
         if (storage.update(user) == null) {
+            log.info("Cannot update because user with id={} not found", user.getId());
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
         return user;
