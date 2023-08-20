4e9d995e7252dbea905d99e4ca185f77cc36f39f
Artem <av.cherep@yandex.ru>
2023-08-20 15:54:28 +0300

    feat: добавить тесты для валидации фильмов

---
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmValidationTest.java
@@ -0,0 +1,107 @@
+package ru.yandex.practicum.filmorate.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmValidationTest {
+
+    private static Validator validator;
+    private Film film;
+
+    @BeforeAll
+    static void beforeAll() {
+        try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            validator = validatorFactory.getValidator();
+        }
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        film = new Film()
+                .setName("Pulp Fiction")
+                .setDescription("Pulp Fiction film description")
+                .setReleaseDate(LocalDate.of(1994, 5, 21))
+                .setDuration(Duration.ofMinutes(154));
+    }
+
+    @Test
+    void shouldValidateFilm() {
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldNotValidateFilmWithBlankName() {
+        film.setName("");
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void shouldValidateFilmWithMaxDescription() {
+        film.setDescription("A".repeat(200));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldNotValidateFilmWithLongDescription() {
+        film.setDescription("A".repeat(201));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void shouldValidateFilmWithBirthdayDay() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 28));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    void shouldNotValidateFilmWithWrongDate() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 27));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void shouldNotValidateFilmWithNegativeDuration() {
+        film.setDuration(Duration.ofMinutes(-10));
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty());
+    }
+
+    @Test
+    void shouldNotValidateFilmWithZeroDuration() {
+        film.setDuration(Duration.ZERO);
+
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty());
+    }
+}
