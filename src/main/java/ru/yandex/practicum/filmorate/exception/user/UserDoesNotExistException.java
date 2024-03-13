096dfe73591d7d2d3028eb5c32734a6f56130c10
SergeyIU <Murmansk.axsel@yandex.ru>
2024-03-13 12:51:16 +0300

    Лента событий

--- src/main/java/ru/yandex/practicum/filmorate/exception/user/UserDoesNotExistException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/user/UserDoesNotExistException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception.user;
 
-public class UserDoesNotExistException extends RuntimeException{
+public class UserDoesNotExistException extends RuntimeException {
     public UserDoesNotExistException(String message) {
         super(message);
     }
